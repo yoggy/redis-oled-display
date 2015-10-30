1223a84e6eea4a0ff0ce2cce74baca6569151d30
yoggy <yoggy0@gmail.com>
2015-10-30 15:14:16 +0900

    modified:   raspi-status.rb

--- sample/raspi-status.rb
+++ sample/raspi-status.rb
@@ -13,8 +13,13 @@ r = Redis.new
 count = 0
 
 loop do
-  r.set "oled:0", `hostname`.chomp + " status" + (count % 2 == 0 ? "." : "")
-  r.set "oled:1", Time.now.strftime("%Y/%m/%d %H:%M")
+  t = Time.now
+  t_str  = t.strftime("%Y/%m/%d %H")
+  t_str += count % 2 == 0 ? ":" : " "
+  t_str += t.strftime("%M")
+  
+  r.set "oled:0", `hostname`.chomp + " status"
+  r.set "oled:1", t_str
   r.set "oled:2", `LANG=C /sbin/ifconfig | grep -v 127.0.0.1 | grep inet | awk '{print $2}' | sed -e 's/addr://'`
   
   count += 1
