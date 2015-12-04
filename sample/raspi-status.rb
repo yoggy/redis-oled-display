45bf1c1eab9ebc84cbe011b83288b43b04af2c2e
yoggy <yoggy0@gmail.com>
2015-12-04 14:30:40 +0900

    modified:   raspi-status.rb

--- sample/raspi-status.rb
+++ sample/raspi-status.rb
@@ -18,7 +18,7 @@ loop do
   t_str += count % 2 == 0 ? ":" : " "
   t_str += t.strftime("%M")
   
-  r.set "oled:0", `hostname`.chomp.split(".")[0] + " status"
+  r.set "oled:0", `hostname -s`.chomp + " status"
   r.set "oled:1", t_str
   r.set "oled:2", `LANG=C /sbin/ifconfig | grep -v 127.0.0.1 | grep inet | awk '{print $2}' | sed -e 's/addr://'`
   
